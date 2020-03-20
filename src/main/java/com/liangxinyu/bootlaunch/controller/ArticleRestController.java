bade2b37fd5ee79ff0c882ae4ec385d27ed201f7
InfiniteAngel <1067750999@qq.com>
2020-03-20 23:46:09 +0800

    5.使用Swagger2构建API文档

--- src/main/java/com/liangxinyu/bootlaunch/controller/ArticleRestController.java
+++ src/main/java/com/liangxinyu/bootlaunch/controller/ArticleRestController.java
@@ -3,6 +3,7 @@ package com.liangxinyu.bootlaunch.controller;
 import com.liangxinyu.bootlaunch.model.AjaxResponse;
 import com.liangxinyu.bootlaunch.model.Article;
 import com.liangxinyu.bootlaunch.service.ArticleTestService;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +18,16 @@ public class ArticleRestController {
     //@RequestMapping(value = "/article",method = POST,produces = "applicatioin/json")
     @Resource
     ArticleTestService articleTestService;
+
+    @ApiOperation(value = "添加文章", notes = "添加新的文章", tags = "Article",httpMethod = "POST")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "title", value = "文章标题", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "content", value = "文章内容", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "author", value = "文章作者", required = true, dataType = "String")})
+    @ApiResponses({
+            @ApiResponse(code=200,message="成功",response=AjaxResponse.class),
+            @ApiResponse(code=400,message="用户输入错误",response=AjaxResponse.class),
+            @ApiResponse(code=500,message="系统内部错误",response=AjaxResponse.class)})
     @PostMapping("/article")
     public AjaxResponse saveArticle(@RequestBody  Article article){
         /*接受表单提交，可用@RequestParam*/
