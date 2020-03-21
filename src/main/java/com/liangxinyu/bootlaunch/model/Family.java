fd7c83a88a54a15e1fc6c02059292302f78a6838
InfiniteAngel <1067750999@qq.com>
2020-03-21 11:26:25 +0800

    6.YAML语法和获取配置的两种方式

---
+++ src/main/java/com/liangxinyu/bootlaunch/model/Family.java
@@ -0,0 +1,20 @@
+package com.liangxinyu.bootlaunch.model;
+
+import lombok.Data;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "family")
+public class Family {
+   /*
+     *使用@Value获取配置值
+     *@Value("${family.family-name}")*/
+    private String familyName;
+    private Father father;
+    private Mother mother;
+    private Child child;
+}
---
+++ src/main/java/com/liangxinyu/bootlaunch/model/Father.java
@@ -0,0 +1,9 @@
+package com.liangxinyu.bootlaunch.model;
+
+import lombok.Data;
+
+@Data
+public class Father {
+    private String name;
+    private Integer age;
+}
